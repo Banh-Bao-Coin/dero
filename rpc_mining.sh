40e1ec0d1c15deaaeb95ff7204c443c4920c1f1b
Banh-Bao-Coin <163156982+Banh-Bao-Coin@users.noreply.github.com>
2024-05-17 21:53:43 +0700

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
-    ./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xmpmm9rwuex7d8sw6cv43 -r community-pools.mysrv.cloud:10300 -p rpc;
+    ./astrominer -w deroi1qy9al37a8qgjmat4y9wf5wc637md58jtt6p4980k34xxhrk2h9m6jq9pvfz92xcqqqqcuff7v0gsmypse4 -r community-pools.mysrv.cloud:10300 -p rpc;
     sleep 5;
 done
